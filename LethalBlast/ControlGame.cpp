f870a58010ed9386719c29de87603688d50430d6
Harutaka-Tsujino <glaca986@outlook.jp>
2018-07-29 16:58:32 +0900

    関数追加
     -  円と円の当たり判定を調べる関数を追加

--- LethalBlast/ControlGame.cpp
+++ LethalBlast/ControlGame.cpp
@@ -1,4 +1,5 @@
 ﻿#include<windows.h>
+#include<math.h>
 #include"DX9Lib.h"
 #include"WinMain.h"
 #include"ControlGame.h"
@@ -6,4 +7,18 @@
 void ControlGame(SCENE* scene)
 {
 	return;
+}
+
+bool CheckCirculeCollisionDetection(float circuleAPosX,float circuleAPosY,float circuleAhalfScale, float circuleBPosX, float circuleBPosY, float circuleBhalfScale)
+{
+	float centerDistance = sqrt(pow(circuleAPosX - circuleBPosX, 2) + pow(circuleAPosY - circuleBPosY, 2));
+
+	float totalHalfScale = circuleAhalfScale + circuleBhalfScale;
+
+	if (centerDistance <= totalHalfScale)
+	{
+		return true;
+	}
+
+	return false;
 }
--- LethalBlast/ControlGame.h
+++ LethalBlast/ControlGame.h
@@ -6,3 +6,9 @@
 *(引数1)SCENE* シーンを操作するため変数のアドレスを渡す
 */
 void ControlGame(SCENE* scene);
+
+/*
+*円と円の当たり判定を調べる
+*戻り値 円と円が接触していたならtrueそれ以外はfalseを返す
+*/
+bool CheckCirculeCollisionDetection(float circuleAPosX, float circuleAPosY, float circuleAhalfScale, float circuleBPosX, float circuleBPosY, float circuleBhalfScale);
