eca9d4351a5caadda3b6defe0724bd14806f2b35
Harutaka-Tsujino <glaca986@outlook.jp>
2018-07-29 22:22:03 +0900

    列挙体の作成
     -  物理 特殊の属性列挙体を作成

--- LethalBlast/WinMain.h
+++ LethalBlast/WinMain.h
@@ -15,6 +15,20 @@ enum SCENE
 	SCENE_MAX
 };
 
+//炎属性などの特殊属性列挙体
+enum ELEMENT_ATTRIBUTE
+{
+	//特殊属性の最大値
+	ELEMENT_ATTRIBUTE_MAX
+};
+
+//武器などによる物理属性列挙体
+enum ATTACK_ATTRIBUTE
+{
+	//物理属性の最大数
+	ATTACK_ATTRIBUTE_MAX
+};
+
 //シーンを操作する
 extern SCENE scene;
 
