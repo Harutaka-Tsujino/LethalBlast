c8357ddf2531ad37d1a1bb5673b22a5f53db282e
Harutaka-Tsujino <glaca986@outlook.jp>
2018-08-01 00:27:22 +0900

    列挙体追加
     -  特殊を及び物理属性の列挙体作成

--- LethalBlast/WinMain.h
+++ LethalBlast/WinMain.h
@@ -18,15 +18,45 @@ enum SCENE
 //炎属性などの特殊属性列挙体
 enum ELEMENT_ATTRIBUTE
 {
+	//火属性
+	FIRE_ELEMENT,
+
+	//水属性
+	WATER_ELEMENT,
+
+	//風属性
+	WIND_ELEMENT,
+
+	//光属性
+	SHINING_ELEMENT,
+
+	//闇属性
+	DARKNESS_ELEMENT,
+
+	//属性なし
+	VOID_ELEMENT,
+	
 	//特殊属性の最大値
-	ELEMENT_ATTRIBUTE_MAX
+	ELEMENT_ATTRIBUTES_MAX
 };
 
 //武器などによる物理属性列挙体
 enum ATTACK_ATTRIBUTE
 {
+	//斬属性
+	SLASH_ATTACK,
+
+	//打属性
+	SMASH_ATTACK,
+
+	//貫属性
+	PENETRATION_ATTACK,
+
+	//属性なし
+	VOID_ATTACK,
+
 	//物理属性の最大数
-	ATTACK_ATTRIBUTE_MAX
+	ATTACK_ATTRIBUTES_MAX
 };
 
 //シーンを操作する
