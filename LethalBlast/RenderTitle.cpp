4e22a566f1d212a5a28f592cf6cdb3a9db6aa286
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2018-08-04 15:41:09 +0900

    タイトルの制御と描画の追加

---
+++ LethalBlast/RenderTitle.cpp
@@ -0,0 +1,93 @@
+﻿#include"RenderTitle.h"
+
+void TitleRender(SCENE *scene)
+{
+	static TEXTUREID textureIds[TITLE_TEX_MAX];
+	static FONTID fontIds[TITLE_FONT_MAX];
+
+	TitleRenderInit(textureIds);
+
+	CustomVertex TitleBackGround[4];
+
+	CustomImageVerticies(TitleBackGround, DISPLAY_WIDTH / 2.f, DISPLAY_HEIGHT / 2.f, DISPLAY_WIDTH / 2.f, DISPLAY_HEIGHT / 2.f);
+	DrawImage(TitleBackGround, textureIds[TITLE_BG_TEX]);
+
+	TitleRogoCanFadeIn(textureIds);
+
+	PressEnterFont(fontIds);
+
+}
+
+void TitleRenderInit(TEXTUREID* textureIds)
+{
+	static bool isFirstFrame = true;
+
+	//一回だけ読み込む
+	if (isFirstFrame)
+	{
+		RoadTexture("Texture/TitleBackGround.png", &textureIds[TITLE_BG_TEX]);
+		RoadTexture("Texture/TitleRogo.png", &textureIds[TITLE_ROGO_TEX]);
+
+		isFirstFrame = false;
+	}
+}
+
+void TitleRogoCanFadeIn(TEXTUREID* textureIds)
+{
+	CustomVertex TitleRogo[4];
+
+	//タイトルロゴのｘ、y座標
+	float TetleRogoPosX = DISPLAY_WIDTH / 2.f;
+	float TetleRogoPosY = DISPLAY_HEIGHT / 2.5f;
+
+	//アルファ値を変えるためのカウント
+	static int alphaCount = 0.f;
+
+	//透明度の変数
+	static float alpha = 0;
+	
+	//ここからフェードイン処理
+	alphaCount++;
+
+	if (alphaCount <= 75)
+	{
+		alpha += 255 / 75;
+	}
+	//ここまでフェードイン処理
+
+	CustomImageVerticies(TitleRogo, TetleRogoPosX, TetleRogoPosY, 600.f, 600.f, GetColor(alpha, 0xFF, 0xFF, 0xFF));
+	DrawImage(TitleRogo, textureIds[TITLE_ROGO_TEX]);
+}
+
+void PressEnterFont(FONTID* fontIds)
+{
+	//アルファ値を変えるためのカウント
+	static int alphaCount = 0;
+
+	//透明度の変数
+	static float alpha = 0;
+
+	//ここから点滅処理
+	if (alphaCount > 75 && alphaCount <= 150)
+	{
+		alpha += 255 / 75;	//透明度を上げていく
+	}
+
+	if (alphaCount > 150 && alphaCount <= 225)
+	{
+		alpha -= 255 / 75;	//透明度を下げていく
+	}
+
+	//カウントが255になったら初期化する
+	if (alphaCount == 225)
+	{
+		alphaCount = 75;
+	}
+
+	alphaCount++;
+
+	//ここまで点滅処理
+
+	SetFont(50.f, 50.f, "MS ゴシック", fontIds, 20.f);
+	WriteText(DISPLAY_WIDTH / 2.f, 700.f, "Press Enter", DT_CENTER, fontIds[PRESS_ENTER_FONT], GetColor(alpha, 0xFF, 0xFF, 0xFF));
+}
---
+++ LethalBlast/RenderTitle.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include"WinMain.h"
+#include"ControlTitle.h"
+#include"DX9Lib.h"
+
+enum TITLETEX
+{
+	//タイトル背景
+	TITLE_BG_TEX,
+
+	//タイトルロゴ
+	TITLE_ROGO_TEX,
+
+	//タイトルで使う画像の最大枚数
+	TITLE_TEX_MAX,
+};
+
+enum TITLEFONT
+{
+	//"PRESS ENTER"の文字フォント
+	PRESS_ENTER_FONT,
+
+	//タイトルで使うフォントの最大数
+	TITLE_FONT_MAX,
+};
+
+/*
+*メインのタイトルの描画関係の処理を行う関数
+*(引数1)SCENE* シーンを操作するため変数のアドレスを渡す
+*/
+void TitleRender(SCENE *scene);
+
+/*
+*画像読み込みを行う関数
+*(引数1)TEXTUREID*　配列の中身に読み込む画像を渡す
+*/
+void TitleRenderInit(TEXTUREID* textureIds);
+
+/*
+*タイトルロゴをフェードインの描画する関数
+*(引数1)TEXTUREID*　配列の中身に読み込む画像を渡す
+*/
+void TitleRogoCanFadeIn(TEXTUREID* textureIds);
+
+/*
+*"press enter"の文字を描画する関数
+*点滅処理も含まれてる
+*(引数1)FONTID* 配列の中身に読み込む文字を出す
+*/
+void PressEnterFont(FONTID* fontIds);
