bf9e4a1de5a3672bf488733eabf20b60ebc866ae
Harutaka-Tsujino <glaca986@outlook.jp>
2018-08-12 17:39:50 +0900

    ファイルの追加
     -  新規ソースコードの追加及びプロジェクトファイルの更新

---
+++ LethalBlast/ControlMagicKnightMainGame.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+//魔法剣士の単語
+enum MAGIC_KNIGHT_WORD
+{
+	VOID_WORD,
+#include "Files/Enum/MagicKnightWordEnum.txt"
+};
+
+#define DECK_NAME_MAX 20 
+#define DECK_WORD_MAX 40
+typedef struct
+{
+	char m_name[DECK_NAME_MAX];
+	int m_wordIds[DECK_WORD_MAX];
+	int m_cost;
+}MAGIC_KNIGHT_DECK;
+
+typedef struct
+{
+	int currentDeckId;
+	int m_deckList
+
+}MAGIC_KNIGHT_CHOSE_DECK;
