64e29d9b97148ab56d970fbd76c684a8c6faa590
Harutaka-Tsujino <glaca986@outlook.jp>
2018-08-13 20:44:09 +0900

    #12 ゲーム開始時の処理
     -  デッキのと手札の単語の準備処理の実装

--- LethalBlast/ControlMagicKnightMainGame.h
+++ LethalBlast/ControlMagicKnightMainGame.h
@@ -7,18 +7,87 @@ enum MAGIC_KNIGHT_WORD
 #include "Files/Enum/MagicKnightWordEnum.txt"
 };
 
-#define DECK_NAME_MAX 20 
+//単語の名前の文字数最大値
+#define WORD_NAME_CHAR_MAX 40
+
+//単語のデータ
+typedef struct
+{
+	//名前
+	char m_word[WORD_NAME_CHAR_MAX];
+
+	//特殊属性
+	ELEMENT_ATTRIBUTE m_element;
+
+	//物理属性
+	ATTACK_ATTRIBUTE m_attack;
+
+	//特殊効果ビットフラグ
+	unsigned long m_specialAbilities;
+}WordData;
+
+//デッキの名前の最大文字数
+#define DECK_NAME_CHAR_MAX 40
+
+//デッキに入れることができる単語の最大数
 #define DECK_WORD_MAX 40
+
+//デッキのデータ
 typedef struct
 {
-	char m_name[DECK_NAME_MAX];
+	//デッキの名前
+	char m_name[DECK_NAME_CHAR_MAX];
+
+	//デッキを構成している単語の識別子
 	int m_wordIds[DECK_WORD_MAX];
-	int m_cost;
-}MAGIC_KNIGHT_DECK;
 
+	//特殊属性別の合計数
+	int m_elementTotals[ELEMENT_ATTRIBUTES_MAX];
+
+	//物理属性別の合計数
+	int m_attackTotals[ATTACK_ATTRIBUTES_MAX];
+
+	//構成する単語の数
+	int m_wordNum;
+
+	//コスト
+	int m_cost;
+}MagicKnightDeck;
+
+//単語の手札最大数
+#define HAND_WORD_MAX 15
+
+//プレイング時のデッキデータ
 typedef struct
 {
-	int currentDeckId;
-	int m_deckList
+	//現在のデッキの識別子
+	int currentId;
 
-}MAGIC_KNIGHT_CHOSE_DECK;
+	//手札の単語の識別子
+	int handWordId[DECK_WORD_MAX];
+
+	//デッキにある単語の識別子
+	int deckWordId[HAND_WORD_MAX];
+
+	//墓地にある単語の識別子
+	int discardWordId[DECK_WORD_MAX];
+}MagicKnightPlayingDeck;
+
+//必殺技を構成する単語の最大数
+#define MAGIC_KNIGHT_ACTION_COMPONENT_WORDS_MAX 5
+
+//魔法剣士の必殺技のデータ
+typedef struct
+{
+	//構成している単語の識別子
+	MAGIC_KNIGHT_WORD m_componentWordIds[MAGIC_KNIGHT_ACTION_COMPONENT_WORDS_MAX];
+
+	//特殊属性別の合計数
+	char m_elemetalTotals[ELEMENT_ATTRIBUTES_MAX];
+
+	//物理属性別の合計数
+	char m_AttackTotals[ATTACK_ATTRIBUTES_MAX];
+
+	//特殊効果ビットフラグ
+	unsigned long m_specialAbilities;
+}MagicKnightAction;
--- LethalBlast/ControlMagicKnigtMainGame.cpp
+++ LethalBlast/ControlMagicKnigtMainGame.cpp
@@ -1,28 +1,103 @@
 ﻿#include"DX9Lib.h"
 #include"WinMain.h"
+#include"ControlCharaChoice.h"
 #include"ControlMagicKnightMainGame.h"
 
 void ControlMagicKnightMainGame()
 {
+	//デッキの保存できる最大数
 	const int MAGIC_KNIGHT_DECKS_MAX = 15;
-	static MAGIC_KNIGHT_DECK magicKnightDecks[MAGIC_KNIGHT_DECKS_MAX];
+	static MagicKnightDeck magicKnightDecks[MAGIC_KNIGHT_DECKS_MAX];
 
-	//現在選択されている単語(列挙子)のデッキをシャッフルして格納する
-	static int wordDeck[DECK_WORD_MAX];
+	MagicKnightPlayingDeck magicKnightPlayingDeck;
 
-	const int FIRST_FRAME = -1;
-	static int frameCount = FIRST_FRAME;
+	MagicKnightAction magicKnightAction;
 
-	if (frameCount == FIRST_FRAME)
+	//初期化を行うときのフレームカウント
+	const int INIT_FRAME = -1;
+
+	//フレームのカウント
+	static int frameCount = INIT_FRAME;
+
+	//初期化を行う
+	if (frameCount == INIT_FRAME)
 	{
+		//フレームカウントを0にすることによって後の計算時が簡単に行える
 		frameCount = 0;
 
-		memset(magicKnightDecks, 0, sizeof(int)*DECK_WORD_MAX);
-		memset(wordDeck, 0, sizeof(int)*DECK_WORD_MAX);
+		//構造体の初期化 全て0を代入する
+		memset(magicKnightDecks, 0, sizeof(MagicKnightDeck));
+		memset(&magicKnightPlayingDeck, 0, sizeof(MagicKnightPlayingDeck));
+		memset(&magicKnightAction,0,sizeof(MagicKnightAction));
 
-		for (int deckNum = 0; deckNum < DECK_WORD_MAX; deckNum++)
+		//デッキの読み込み あとで書く
+
+		//デッキを選ばせる あとで書く
+
+		/*プレイ開始時の手札やデッキなどの初期化 開始*/
+		int* pWordShuffleBuf = (int*)malloc(sizeof(int)*magicKnightDecks[magicKnightPlayingDeck.currentId].m_wordNum);
+
+		//重複しないようにするためデッキの単語識別子をバッファーに保存する
+		for (int deckWordNum = 0; deckWordNum < magicKnightDecks[magicKnightPlayingDeck.currentId].m_wordNum; ++deckWordNum)
 		{
-			wordDeck[deckNum] = magicKnightDecks[].m_wordIds[]
+			pWordShuffleBuf[deckWordNum] = magicKnightDecks[magicKnightPlayingDeck.currentId].m_wordIds[deckWordNum];
 		}
+
+		//デッキの中の単語識別子をシャッフルする
+		for (int deckWordNum = 0; deckWordNum < DECK_WORD_MAX; deckWordNum++)
+		{
+			int unshuffledDeckWordNum = magicKnightDecks[magicKnightPlayingDeck.currentId].m_wordNum - deckWordNum;
+			int chosenNum = rand() % (unshuffledDeckWordNum);
+
+			magicKnightPlayingDeck.deckWordId[deckWordNum] =pWordShuffleBuf[chosenNum];
+
+			//一度選ばれたものは選ばれる範囲から外す
+			for (int backShift = chosenNum; backShift < unshuffledDeckWordNum; ++backShift)
+			{
+				pWordShuffleBuf[backShift] = pWordShuffleBuf[backShift + 1];
+			}
+		}
+
+		//開始のドロー
+		for (int drawNum = 0; drawNum < HAND_WORD_MAX; ++drawNum)
+		{
+			magicKnightPlayingDeck.handWordId[drawNum] = magicKnightPlayingDeck.deckWordId[0];
+
+			//ドローされたら残りの単語識別子を手前にずらす
+			for (int backShift = 0; backShift < HAND_WORD_MAX- drawNum; ++backShift)
+			{
+				magicKnightPlayingDeck.deckWordId[backShift] = magicKnightPlayingDeck.deckWordId[backShift + 1];
+			}
+		}
+		/*プレイ開始時の手札やデッキなどの初期化 終了*/
 	}
+
+	/*カーソルとの当たり判定用の頂点設定 開始*/
+	//手札
+	const int RECT_VERTEX_NUM = 4;
+	CustomVertex handWordCollisionsVertex[HAND_WORD_MAX*RECT_VERTEX_NUM];
+
+	const int HAND_WORD_COLLISION_WIDTH = DISPLAY_WIDTH / 10;
+	const int HAND_WORD_COLLISION_HEIGHT = DISPLAY_HEIGHT / 20;
+	for (int handWord = 0; handWord < HAND_WORD_MAX; ++handWord)
+	{
+		CustomImageVerticies(&handWordCollisionsVertex[handWord*RECT_VERTEX_NUM],
+			DISPLAY_WIDTH - HAND_WORD_COLLISION_WIDTH * (handWord % 2 + 1), HAND_WORD_COLLISION_HEIGHT*(handWord % 8), 
+			HAND_WORD_COLLISION_WIDTH, HAND_WORD_COLLISION_HEIGHT);
+	}
+
+	//必殺技
+	CustomVertex magicKnightActionCollisionsVertex[MAGIC_KNIGHT_ACTION_COMPONENT_WORDS_MAX*RECT_VERTEX_NUM];
+
+	const int ACTION_COMPONENT_WORD_COLLISION_WIDTH = DISPLAY_WIDTH / 20;
+	const int ACTION_COMPONENT_WORD_COLLISION_HEIGHT = DISPLAY_HEIGHT / 40;
+	for (int actionComponentWord = 0; actionComponentWord < MAGIC_KNIGHT_ACTION_COMPONENT_WORDS_MAX; ++actionComponentWord)
+	{
+		CustomImageVerticies(&magicKnightActionCollisionsVertex[actionComponentWord*RECT_VERTEX_NUM],
+			(DISPLAY_WIDTH / 2) + ACTION_COMPONENT_WORD_COLLISION_WIDTH, DISPLAY_HEIGHT - ACTION_COMPONENT_WORD_COLLISION_HEIGHT,
+			ACTION_COMPONENT_WORD_COLLISION_WIDTH, ACTION_COMPONENT_WORD_COLLISION_HEIGHT);
+	}
+	/*カーソルとの当たり判定用の頂点設定 終了*/
+
+
 }
