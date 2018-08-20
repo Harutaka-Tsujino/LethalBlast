cb1e0d765254622aa1740537ed92a77ed5257649
Harutaka-Tsujino <glaca986@outlook.jp>
2018-08-20 14:06:57 +0900

    #3 アイテムドロップ
     -  アイテムのドロップを操作する関数

---
+++ LethalBlast/ControlItemDrop.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include"DX9Lib.h"
+#include"WinMain.h"
+#include"ControlMagicKnightMainGame.h"
+
+/*
+アイテムのドロップを操作する関数
+ワードデータ構造体の先頭アドレス
+ドロップする単語の列挙子 ファイアーとか
+パーセントでの確率100って入れたら絶対出る1って入れたら１パ－
+*/
+bool CheckDropItem(WordData* pMagicKnightWordDatas, MAGIC_KNIGHT_WORD wordId, int probabilityParcent);
